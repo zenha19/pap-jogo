ad698d2ed6b72f2efae7f75cc116c0562108a85e
zenha19 <jonnygabiazenha@gmail.com>
2026-01-20 21:25:41 +0000

    Add files via upload

--- area de testes/movimentação basica/assets/NewMonoBehaviourScript.cs
+++ area de testes/movimentação basica/assets/NewMonoBehaviourScript.cs
@@ -5,7 +5,9 @@ public class NewMonoBehaviourScript : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
 
+    public float velocidadeDoJogador;
 
+    public Rigidbody2D origidBody2d;
 
 
     void Start()
@@ -16,6 +18,13 @@ public class NewMonoBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-                
+          
     }
+     void FixedUpdate()
+     {
+        float inputDoMovimento =Input.GetAxisRaw("Horizontal");
+        origidBody2d.linearVelocity= new Vector2(inputDoMovimento * velocidadeDoJogador, origidBody2d.linearVelocity.y);
+       
+     }
+
 }
